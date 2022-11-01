77f1786a3a3d48ba88e5e72fcff097d70322a070
Majidli Nijat <nijatmajidli@gmail.com>
2022-11-01 22:33:34 +0100

    new modif

--- assets/app.js
+++ assets/app.js
@@ -8,8 +8,6 @@
 // any CSS you import will output into a single css file (app.css in this case)
 import './styles/app.scss';
 
-import './styles/responsive-design.scss';
-
 // start the Stimulus application
 import './bootstrap';
 
